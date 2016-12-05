b24824ce9e3cea91df06d9ec035379503f7e2e10
mathieu le gueux <mathieu@Bruce-Lit>
2016-12-05 09:18:20 +0100

    re

--- Kernel/src/main.c
+++ Kernel/src/main.c
@@ -18,7 +18,7 @@
 #include "Test/minunit.h"
 
 #include "Tools/tools.h"
- // dsdspierreke bikooeqs
+ // dsdspierreke bikooeqs last tezts
 /*
  * Description	: This is the only main which will be used in this project
  * Parameters	: None
