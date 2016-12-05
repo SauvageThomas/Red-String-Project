268269fdde619203444dd2fa83aa902c5885c3fc
Pierre le Smer <Koreuc@172.17.56.70>
2016-12-05 08:10:39 +0100

    Project setup on my environment

--- Kernel/src/main.c
+++ Kernel/src/main.c
@@ -26,15 +26,6 @@
  */
 int main(void) {
 
-	char src[50], dest[50];
-
-	strcpy(src, "This is source");
-	strcpy(dest, "This is destination");
-
-	strcat(dest, src);
-
-	printf("Final destination string : |%s|", dest);
-
 	if (DEBUG) {
 		puts("Debug is ON");
 	}
