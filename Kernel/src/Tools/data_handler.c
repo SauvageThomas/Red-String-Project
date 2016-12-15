2c5133b45f4867882e6c8b2cc6bf22bef49cf680
ChambreMichael <chambre.micha@orange.fr>
2016-12-15 09:15:02 +0100

    màj anglais

--- Kernel/src/Tools/data_handler.c
+++ Kernel/src/Tools/data_handler.c
@@ -17,7 +17,7 @@ int file_length_retriever( char* path){
 		fseek(file, 0, SEEK_SET);
 		fclose(file);}
 	else 
-		printf("Le fichier %s n'a pas pu être ouvert ou n'existe pas !\n", path);
+		printf("the file  %s can't be opened or doesn't exist !\n", path);
 	return size;
 }
 
@@ -40,18 +40,33 @@ char* read_string_from_file(char *path){
 		return string_in_file;
 	}
 	else
-		printf("Le fichier %s n'a pas pu être ouvert ou n'existe pas !\n", path);
-	return "vide !";
+		printf("the file  %s can't be opened or doesn't exist !\n", path);
+	return "empty !";
 }
 
 
 void write_string_in_file(char *path, char *string) {
 	FILE * file= NULL;
-	file=fopen (path, "w+");// si on veut mettre à jour, sinon juste w
+	file=fopen (path, "w+");// if we want to update, else just w
 	if (file!=NULL){
 		fputs( string, file);}
 	else
-		printf("Le fichier %s n'a pas pu être crée ou l'on a pas pu écrire dedans !\n", path);
+		printf("the file %s can't be created or we can't add text!\n", path);
 	fclose(file);
 }
 
+int main(){
+	int size=file_length_retriever("../Data/Test/test_read_data_from_file_2");
+	char* string_in_file=(char*)malloc((size)*sizeof(char));
+	char* string_in_file2=(char*)malloc((size)*sizeof(char));
+	string_in_file=read_string_from_file("../Data/Test/test_read_data_from_file_2");
+	for (int j=0; j<size; j++)
+		printf("%c", string_in_file[j]);
+	printf("\n");
+	write_string_in_file("../Data/Test/test_read_data_from_file_4", string_in_file);
+	string_in_file2=read_string_from_file("../Data/Test/test_read_data_from_file_4");
+	for (int j=0; j<size; j++)
+		printf("%c", string_in_file2[j]);
+	printf("\n");
+	return 0;
+}
