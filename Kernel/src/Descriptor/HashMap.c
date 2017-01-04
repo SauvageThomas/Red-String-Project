290d9c38480809ab68ed7924e271fc45c33d95c6
Pierre <pierre31m@gmail.com>
2017-01-04 17:56:12 +0100

    HashMap version stable

--- Kernel/src/Descriptor/HashMap.c
+++ Kernel/src/Descriptor/HashMap.c
@@ -7,13 +7,26 @@ void init(HashMap* map){
 	*map = NULL;
 }
 
+CellHashMap* findValue(CellHashMap* cell, char* key){
+
+	if(cell == NULL)
+		return NULL;
+	if(cell->key == key)
+		return cell;
+	return findValue(cell->next, key);
+}
+
 void addValue(HashMap* map, char* key, int nbOccurence){
-		
-	HashMap map2 = (HashMap)malloc(sizeof(CellHashMap));
-	((CellHashMap*)map2)->key = key;
-	map2->nbOccurence = nbOccurence;
-	((CellHashMap*)map2)->next = *map;
-	*map = map2;
+	CellHashMap* cellTmp = findValue(*map,key);
+	if(cellTmp == NULL){
+		HashMap map2 = (HashMap)malloc(sizeof(CellHashMap));
+		((CellHashMap*)map2)->key = key;
+		map2->nbOccurence = nbOccurence;
+		((CellHashMap*)map2)->next = *map;
+		*map = map2;
+	}else{
+		cellTmp->nbOccurence+=nbOccurence;
+	}
 }
 
 
@@ -24,13 +37,11 @@ void displayMap(HashMap map){
 	}
 }
 
-
-
-
 int main(){
 	HashMap map;
 	init(&map);
 	addValue(&map, "manger", 1);
+	addValue(&map, "Kebab", 4);
 	addValue(&map, "Kebab", 4);
 	displayMap(map);
 }
