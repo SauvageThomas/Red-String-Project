674fdd3182779e9693c189a1ad20213c274013d9
ChambreMichael <chambre.micha@orange.fr>
2017-01-07 00:58:50 +0100

    text_finder.h  updated

--- Kernel/src/Search/text_finder.h
+++ Kernel/src/Search/text_finder.h
@@ -1,4 +1,4 @@
 
 char * remove_xml(char* content);
 char* remove_punctuation(char* content);
-char** remove_words(char* content, int *matrix_length);
+char** remove_words(char* content, int* matrix_length);
