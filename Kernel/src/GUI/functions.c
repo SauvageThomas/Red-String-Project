5164f4df0d5199f9276306ca83c3572777e1de63
Thomas <Thomas@Thomas-Server>
2017-01-09 23:15:09 +0100

    Minor bug correction

--- Kernel/src/GUI/functions.c
+++ Kernel/src/GUI/functions.c
@@ -30,13 +30,13 @@ void search_by_file(char* path) {
 	puts("Please, enter a file path : ");
 	printf(">>");
 
-	if (get_secure_input(file_name, sizeof(file_name))) {
+	if (get_secure_input(file_name, KSIZE)) {
 		char* full_path = malloc(strlen(file_path) + strlen(file_name));
 
 		strcpy(full_path, file_path);
 		strcat(full_path, file_name);
+		printf("input : %s\n", file_name);
 		printf("full_path : %s\n", full_path);
-		
 
 		int res = search_data(full_path);
 		show_search_report(res);
