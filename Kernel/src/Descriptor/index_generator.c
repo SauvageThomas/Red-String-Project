e46a00a122410deb8fda0fdd5b9496da586dd2ab
Pierre <pierre31m@gmail.com>
2017-01-12 11:12:31 +0100

    Cartouche

--- Kernel/src/Descriptor/index_generator.c
+++ Kernel/src/Descriptor/index_generator.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : index_generator.c
- Author      : Kévin
+ Author      : Pierre
  Version     :
  Description : Generate index file
  ============================================================================
@@ -13,6 +13,19 @@ void update_index(){
 	/*
 	* UPDATE THE INDEX FILE
 	*/
+
+	//TODO: parcourir tous les descriptors 
+	// pour chaque descriptor faire : 
+	// - Creer un hashMap(clé = mot, valeur = nombre d'occurence)
+	// - Pour chaque nouveau mot :
+	//       si première occurence : l'ajouter dans la map avec le mot en clé et 1 en valeur
+	//		 else : incrémenter la valeur d'occurence	
+
+descripteur_test
+
+
+	  
+	
 	puts("INDEX FILE UPDATED.");
 }
 
