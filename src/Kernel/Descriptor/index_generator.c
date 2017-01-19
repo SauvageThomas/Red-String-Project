6e6624c4e4a45b656d90f3ac16ba047dd00cfdfe
Pierre <pierre31m@gmail.com>
2017-01-19 23:41:03 +0100

    index_generator

--- src/Kernel/Descriptor/index_generator.c
+++ src/Kernel/Descriptor/index_generator.c
@@ -11,6 +11,7 @@
 #include "../Tools/data_handler.h"
 #include "../Tools/map_of_map.h"
 #include "../Data/constant.h"
+#include <string.h>
 
 void update_index(){
 
@@ -20,77 +21,92 @@ void update_index(){
 	*/
 
 
-	char* path = "../../res/descriptors/text_descriptors";
-	DataFile file = init_data_file(path);
-	char* text = read_string_from_file(file);
-	//printf("%s\n",read_string_from_file(file));
+	char* pathDescriptor = "../../../data/descriptors/text_descriptors_v2";
+	DataFile fileSource = init_data_file(pathDescriptor);
+	char* text = read_string_from_file(fileSource);
+
+	char* pathIndex = "../../../data/descriptors/text_index";
+	DataFile fileDestination = init_data_file(pathDescriptor);
+
 	char* fileName = malloc(KSIZE +1);
-	char* wordCurent = malloc(KSIZEWORD +1);
-	char* numberOccurenceCurrent = malloc(sizeof(int));
+	char* wordCurent = malloc(KSIZEWORD-50);
+	char* numberOccurenceCurent = malloc(sizeof(int));
+	int intNumberOccurenceCurent;
 	int isFileName = 0;
-	int isDate = 0;
 	int isText = 0;
 	int isWordCurent = 0;
-	int isNumberOccurenceCurrent = 0;
-	/*for(int i = 0; 	 i< file.length; i++){
-		if(text[i]=='>'){
-			isFileName = 1;
-			
-		}
-		if( isFileName == 1 && text[i]=='?'){
-			isFileName = 0;
-			isDate = 1;
-		}
-		if(isDate == 1 && text[i]=='\n')
-			isDate ==0;
-		if(isFileName == 1 )
-			printf("%c",text[i]);
-	}*/
+	int isNumberOccurenceCurent = 0;
 	char* pattern = "%c";
 	MapOfMap map;
 	init_MapOfMap(&map);
-/*
-	for(int i = 0; 	 i< file.length; i++){
-		if(text[i]=='>'){
-			fileName = "";
-			isFileName = 1;
-			
-		}
-		if(isFileName == 1 && text[i]!='?'){
-			printf("%c",text[i]);
-			sprintf(fileName, pattern,text[i]);
-		}
-		if( isFileName == 1 && text[i]=='?'){
-			isFileName = 0;
-			isDate = 1;
-		}
-		if(isDate == 1 && text[i]=='\n'){
-			isDate = 0;
-			isText = 1;
-			isWordCurrent = 1;
-		}
-		
-		if(isText == 1 && isWordCurrent == 1 && text[i] != " "){
-			sprintf(wordCurent, pattern,text[i]);
-		}
-		if(isText == 1 && isWordCurrent == 1 && text[i] == " " ){
-			isWordCurrent = 0;
-			isNumberOccurenceCurrent = 1;
-		}
-		if(isText == 1 && isNumberOccurenceCurrent == 1 && text[i] != '\n'){
-			sprintf(numberOccurenceCurrent, pattern,text[i]);
-		}
-		if(isText == 1 && isNumberOccurenceCurrent == 1 && text[i] == '\n'){
-			add_value_MapOfMap(&map, wordCurent, fileName, strtok(numberOccurenceCurrent));
-		}
+	int test = 0;
+	char* tmp;
 
+	for(int i = 0; 	 i< fileSource.length; i++){
+		if(text[i]=='>'){	
+			test ++;
+			fileName="";
+			fileName =  malloc(KSIZE + 1);
+			isWordCurent = 0;
+			isFileName = 1;
+			continue;
+			
+		}
+		if(isFileName == 1 && text[i]!='\n'){
+			tmp = malloc(sizeof(char));
+			sprintf(tmp,"%c",text[i]);
+			strcat(fileName, tmp);
+			continue;
+		}
+		if( isFileName == 1 && text[i]=='\n'){
+			isFileName = 0;
+			isWordCurent = 1;
+			wordCurent="";
+			wordCurent =  malloc(KSIZEWORD);
+			continue;
 	
-			
-			
+		}
+		if(isWordCurent == 1 && text[i] != ' ' && text[i] != '\n'){
+			tmp = malloc(sizeof(char));
+			sprintf(tmp,"%c",text[i]);
+			strcat(wordCurent, tmp);
+			continue;
+		}
+		if(isWordCurent == 1 && text[i] == ' ' ){
+			isWordCurent = 0;
+			isNumberOccurenceCurent = 1;
+			numberOccurenceCurent="";
+			numberOccurenceCurent =  malloc(KSIZEWORD);
+			continue;
+		}
+		if(isNumberOccurenceCurent == 1 && text[i] != '\n'){
+			tmp = malloc(sizeof(char));
+			sprintf(tmp,"%c",text[i]);
+			strcat(numberOccurenceCurent, tmp);
+			continue;
+		}
+		if(isNumberOccurenceCurent == 1 && text[i] == '\n'){
+			sscanf(numberOccurenceCurent,"%d", &intNumberOccurenceCurent);
+			add_value_MapOfMap(&map, wordCurent, fileName,intNumberOccurenceCurent);
+			isWordCurent = 1;
+			isNumberOccurenceCurent = 0;
+			wordCurent="";
+			wordCurent =  malloc(KSIZEWORD);
+			continue;
+		}
 	}
-*/
+	
+	if(map!=NULL){
+		write_string_in_file(fileDestination, pop_value_MapOfMap(&map));
+		puts("mamamiya");
+	}
+	while(map!=NULL){
+		append_string_in_file(fileDestination,pop_value_MapOfMap(&map)); 
+		puts("heuuu...");
+	}
 	  
 	
+
 	puts("INDEX FILE UPDATED.");
 }
 
