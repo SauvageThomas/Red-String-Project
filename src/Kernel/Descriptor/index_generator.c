1fd5f3c8d1828491d34533d6eca14ee4f311e51b
Pierre <pierre31m@gmail.com>
2017-01-19 20:36:56 +0100

    index_generator

--- src/Kernel/Descriptor/index_generator.c
+++ src/Kernel/Descriptor/index_generator.c
@@ -10,6 +10,7 @@
 #include "index_generator.h"
 #include "../Tools/data_handler.h"
 #include "../Tools/map_of_map.h"
+#include "../Data/constant.h"
 
 void update_index(){
 
@@ -28,21 +29,80 @@ void update_index(){
 	DataFile file = init_data_file(path);
 	char* text = read_string_from_file(file);
 	//printf("%s\n",read_string_from_file(file));
+	char* fileName = malloc(KSIZE +1);
+	char* wordCurent = malloc(KSIZEWORD +1);
+	char* numberOccurenceCurrent = malloc(sizeof(int));
+	int isFileName = 0;
+	int isDate = 0;
+	int isText = 0;
+	int isWordCurent = 0;
+	int isNumberOccurenceCurrent = 0;
+	/*for(int i = 0; 	 i< file.length; i++){
+		if(text[i]=='>'){
+			isFileName = 1;
+			
+		}
+		if( isFileName == 1 && text[i]=='?'){
+			isFileName = 0;
+			isDate = 1;
+		}
+		if(isDate == 1 && text[i]=='\n')
+			isDate ==0;
+		if(isFileName == 1 )
+			printf("%c",text[i]);
+	}*/
+	char* pattern = "%c";
+	MapOfMap map;
+	init_MapOfMap(&map);
+/*
 	for(int i = 0; 	 i< file.length; i++){
-		if(text[i]=='>')
-			printf("----------------------------------\n");
-		printf("%c",text[i]);
-	}
+		if(text[i]=='>'){
+			fileName = "";
+			isFileName = 1;
+			
+		}
+		if(isFileName == 1 && text[i]!='?'){
+			printf("%c",text[i]);
+			sprintf(fileName, pattern,text[i]);
+		}
+		if( isFileName == 1 && text[i]=='?'){
+			isFileName = 0;
+			isDate = 1;
+		}
+		if(isDate == 1 && text[i]=='\n'){
+			isDate = 0;
+			isText = 1;
+			isWordCurrent = 1;
+		}
+		
+		if(isText == 1 && isWordCurrent == 1 && text[i] != " "){
+			sprintf(wordCurent, pattern,text[i]);
+		}
+		if(isText == 1 && isWordCurrent == 1 && text[i] == " " ){
+			isWordCurrent = 0;
+			isNumberOccurenceCurrent = 1;
+		}
+		if(isText == 1 && isNumberOccurenceCurrent == 1 && text[i] != '\n'){
+			sprintf(numberOccurenceCurrent, pattern,text[i]);
+		}
+		if(isText == 1 && isNumberOccurenceCurrent == 1 && text[i] == '\n'){
+			add_value_MapOfMap(&map, wordCurent, fileName, strtok(numberOccurenceCurrent));
+		}
 
+	
+			
+			
+	}
+*/
 	  
 	
 	puts("INDEX FILE UPDATED.");
 }
-
+/*
 int main(){
 	puts("--> test index generator\n");
-	//update_index();
-	MapOfMap map;
+	update_index();
+/*	MapOfMap map;
 	init_MapOfMap(&map);
 	add_value_MapOfMap(&map, "aa", "Fichier1", 11);
 	add_value_MapOfMap(&map, "bb", "Fichier2", 22);
@@ -55,10 +115,11 @@ int main(){
 	add_value_MapOfMap(&map, "bb", "Fichier6", 222);
 	/*while(!is_map_of_map_empty(map)){
 		printf("%s\n",pop_value_map_of_map(&map)); 
-	}	*/
+	}	
 	HashMap hashMap = *get_hashMap_with_key(map, "aa");
 	//printf("Test ----> : %s\n", pop_value_hash_map(&hashMap));
 	//free_map_of_map(map);
-}
+
+}*/
 
 
