ab2dabdd6ec1056a40ab398299ddfcba0d73c8c1
Twarz <Twarz@Twarz-World.home>
2017-02-07 19:39:11 +0100

    first step JNI

---
+++ src/Controller/Controller.java
@@ -0,0 +1,24 @@
+package src.Controller;
+
+public class Controller {
+	
+	public native void search_by_file();
+	public native void update_descriptors();
+	public native void error_config_file();
+	public native void search_by_keyword();
+	public native void modif_config();
+	public native void generate_all_descriptors();
+	public native void reset_config();
+	public native void load_config();
+	public native void display_data_base();
+	public native void remove_path();
+	public native void get_value_of();
+	public native void change_password();
+	public native void login();
+	public native void wip();
+	public native void input_error();
+	public native void xor_crypt();
+	public native void get_input();
+	public native void clear_console();
+	public native void chrono();
+}
