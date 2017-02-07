0da29fb10adbabe19dbe09e482b9fa2f4ade2a05
Thomas <Thomas@Thomas-Server>
2017-02-07 16:39:13 +0100

    merge

--- src/IHM/src/application/Main.java
+++ src/IHM/src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 
+import application.model.XMLHandler;
 import application.view.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -20,6 +21,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Red-String-Project");
+		
+		new XMLHandler("data/1.xml");
 
 		// System.out.println(new File("bear.wav").toURI());
 		// final AudioClip clip = new
