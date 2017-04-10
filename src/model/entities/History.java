a1894f28935208f11e3519ecb031718cd95f5966
twarz <twarz@world>
2017-04-10 16:29:46 +0200

    load and save must be implemented

--- src/model/entities/History.java
+++ src/model/entities/History.java
@@ -9,13 +9,23 @@ public class History {
 	
 	public History() {
 		this.requests = new ArrayList<>();
+		this.load();
 	}
 	
 	public void addRequest(Request request){
 		this.requests.add(request);
+		this.save();
 	}
 	
 	public List<Request> getRequests(){
 		return this.requests;
+	}
+	
+	public void load(){
+		
+	}
+	
+	public void save(){
+		
 	}
 }
