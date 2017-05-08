71c9eb30d30d7aba85762db0130c6f3bafdce6f8
mathieu <mathieu.vermande@gmail.com>
2017-05-08 20:32:52 +0300

    update database w/ threading

--- Software/src/view/controller/ControllerUpdateBD.java
+++ Software/src/view/controller/ControllerUpdateBD.java
@@ -1,6 +1,8 @@
 package view.controller;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXProgressBar;
@@ -10,67 +12,78 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class ControllerUpdateBD extends AnchorPane{
+public class ControllerUpdateBD extends AnchorPane {
 	@FXML
-    private JFXButton updateButton;
+	private JFXButton updateButton;
 	@FXML
-    private JFXButton cancelButton;
+	private JFXButton cancelButton;
 	@FXML
-    private Text titleText;
+	private JFXButton doneButton;
 	@FXML
-    private Text progressText;
+	private Text titleText;
 	@FXML
-    private JFXProgressBar progressBar;
-	
-	public ControllerUpdateBD(final ControllerSoftware controllerSoftware, final Stage stage){
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                "/view/fxml/FXMLUpdateDB.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
+	private Text progressText;
+	@FXML
+	private JFXProgressBar progressBar;
 
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-		
+	public ControllerUpdateBD(final ControllerSoftware controllerSoftware, final Stage stage) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/fxml/FXMLUpdateDB.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+
+		try {
+			fxmlLoader.load();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+
+		final IndexThread indexThread = new IndexThread(controllerSoftware, progressBar, progressText, doneButton, stage);
+
+		stage.setResizable(false);
+		doneButton.setVisible(false);
+		progressBar.setProgress(0.0);
 		progressBar.setStyle("-fx-progress-color: #00BAB5");
 		titleText.setText("Update Database");
-		titleText.setFont(Font.font ("Helvetica", 20));
+		titleText.setFont(Font.font("Helvetica", 20));
 		titleText.setFill(Color.web("#00BAB5"));
-		
+
 		progressText.setText("");
-		progressText.setFont(Font.font ("Helvetica", 15));
+		progressText.setFont(Font.font("Helvetica", 15));
 		progressText.setFill(Color.web("#00BAB5"));
-		
+
 		updateButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-            	progressBar.setProgress(0.05);
-            	progressText.setText("Indexing text files");
-            	controllerSoftware.indexText();
-            	progressBar.setProgress(0.55);
-            	progressText.setText("Indexing image files");
-            	controllerSoftware.indexImage();
-            	progressBar.setProgress(0.70);
-            	progressText.setText("Indexing sound files");
-            	controllerSoftware.indexSound();
-            	progressBar.setProgress(1.0);
-            	stage.close();
-            }
-        });
-		
+			@Override
+			public void handle(ActionEvent e) {
+				updateButton.setDisable(true);
+				cancelButton.setDisable(true);
+				synchronized (indexThread) {
+					progressBar.setProgress(0.05);
+					progressText.setText("Indexing text files");
+					indexThread.start();
+
+				}
+			}
+		});
+
 		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-            	stage.close();
-            }
-        });
+			@Override
+			public void handle(ActionEvent e) {
+				stage.close();
+			}
+		});
+		
+		doneButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				stage.close();
+			}
+		});
 	}
 }
