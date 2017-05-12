aba42eaebc393f5c108da5ebdfeea1a79061fb54
Twarz <Twarz@Twarz-World.home>
2017-05-12 17:20:06 +0200

    Comment all view package

--- Software/src/view/console/ViewMainMenu.java
+++ Software/src/view/console/ViewMainMenu.java
@@ -21,21 +21,26 @@ public class ViewMainMenu extends ViewMenu implements Observer{
 		this.viewSettings = new ViewSettings(controllerSoftware);
 		this.viewLogin = new ViewLogin(controllerSoftware);
 		this.controllerSoftware = controllerSoftware;
+		// le menu principale observe la base de donnée (CheckDataBase.java)
 		this.controllerSoftware.setObserver(this);
+		// lance l'indexation de la base de donnée (Phase 1 : projet Kernel + JNI)
 		this.updateSoftware();
 	}
 
 	private void updateSoftware() {
 		
 		System.out.print("TEXT  INDEXATION...");
+		// indexe les textes (Phase 1 : projet Kernel + JNI)
 		int res_txt = this.controllerSoftware.indexText();
 		System.out.println("  Done ! (file updated = " + res_txt + ")");
 		
 		System.out.print("IMAGE INDEXATION...");
+		// indexe les images (Phase 1 : projet Kernel + JNI)
 		int res_img = this.controllerSoftware.indexImage();
 		System.out.println("  Done ! (file updated = " + res_img + ")");
 		
 		System.out.print("SOUND INDEXATION...");
+		// indexe les sons (Phase 1 : projet Kernel + JNI)
 		int res_sound =  this.controllerSoftware.indexSound();
 		System.out.println("  Done ! (file updated = " + res_sound + ")");
 		
@@ -68,13 +73,14 @@ public class ViewMainMenu extends ViewMenu implements Observer{
 				this.viewLogin.showView();
 			break;
 		case 0 :
+			// le logiciel termine son exécution, on stop le(s) threads secondaires
 			this.controllerSoftware.stop();
 			System.out.println("QTIL SOftware is shutting down, please wait...");
 			try {
+				// 5s de délai pour l'arret des threads ( 5s = temps de repos d'un thread)
 				Thread.sleep(5000);
 				System.out.println("Good bye ! :)");
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			break;
@@ -83,6 +89,7 @@ public class ViewMainMenu extends ViewMenu implements Observer{
 
 	@Override
 	public void update(Observable from, Object obj) {
+		// affiche un warning lors d'un changement de la BDD (via Observer - Observable)
 		if (from instanceof CheckDataBase)
 			System.out.println("[WARNING] : data base has changed !");
 	}
