f7ba84f4f2e4b8340ea23e9ccd6ca9f7aa0bc01d
Twarz <Twarz@Twarz-World.home>
2017-05-12 18:43:33 +0200

    comments

--- Software/src/model/entities/Setting.java
+++ Software/src/model/entities/Setting.java
@@ -1,25 +1,19 @@
 package model.entities;
 
 public abstract class Setting{
-	/**
-	 * TODO comment
-	 */
+	
 	protected String value;
 	
 	public Setting(String value) {
 		this.value = value;
 	}
 	
-	/**
-	 * TODO comment
-	 */
+	// Retourne la valeur d'un parametre
 	public String getValue(){
 		return this.value;
 	}
 	
-	/**
-	 * TODO comment
-	 */
+	// Change la valeur d'un parametre
 	public void setValue(String value) {
 		this.value = value;
 	}
